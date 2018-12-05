4dc9e9da57258ab5e29a2806ee8957e5a3aed43e
Julien Queffelec <julienqueffelec@macbook-pro-de-julien.home>
2018-12-05 23:21:04 +0100

    add GenerateMessage

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const { generateMessage } = require('./utils/message');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const app = express();
@@ -13,13 +15,21 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
   console.log('new user connected');
 
+  socket.emit('newMessage', generateMessage('admin', 'Bienvenue'));
+
+  socket.broadcast.emit(
+    'newMessage',
+    generateMessage('admin', 'new user joined')
+  );
+
   socket.on('createMessage', message => {
     console.log('createMessage', message);
-    io.emit('newMessage', {
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    /*    socket.broadcast.emit('newMessage', {
       from: message.from,
       text: message.text,
       createAt: new Date().getTime(),
-    });
+    }); */
   });
 
   socket.on('disconnect', () => {
